c8f630d87a87a61ac1c5b88a04fd52396a01cc3a
Rithik Jain <rithik.jain3006@gmail.com>
2020-02-11 11:08:21 +0530

    Generating UUID after the user signs up or is logged in.

--- app/src/main/java/com/dscvit/periodsapp/utils/Constants.kt
+++ app/src/main/java/com/dscvit/periodsapp/utils/Constants.kt
@@ -7,5 +7,6 @@ object Constants {
     const val PREF_NAME = "periods_app_pref"
     const val PREF_AUTH_KEY = "PREF_AUTH_KEY"
     const val PREF_IS_LOGGED_IN = "PREF_IS_LOGGED_IN"
+    const val PREF_DEVICE_ID = "PREF_DEVICE_ID"
 
 }
